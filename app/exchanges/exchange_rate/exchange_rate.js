3d8b738df9038a51c6f7a7e9dce25de78baa985a
Csaba Apagyi <csaba.apagyi@xing.com>
2017-12-29 01:57:53 +0100

    Adds `let`s to variable declaration in exchange_rate

--- app/exchanges/exchange_rate/exchange_rate.js
+++ app/exchanges/exchange_rate/exchange_rate.js
@@ -18,12 +18,12 @@ router.get('/:name/exchange_pairs/:from_currency/:to_currency', function(req, re
 
 function exchange_pairs(exchange, from_currency, to_currency) {
 	return pairs.list(exchange).then(pair_list => {
-		promises = []
-		possible_conversion_pairs = find_conversion_pairs(pair_list, from_currency, to_currency)
+		let promises = []
+		let possible_conversion_pairs = find_conversion_pairs(pair_list, from_currency, to_currency)
 		if(!possible_conversion_pairs) return
 		possible_conversion_pairs.forEach((conversion_pairs, index) => {
-			promise = get_rate(exchange, conversion_pairs).then(rate => {
-				possible_conversion_pairs[index].push(rate)
+			let promise = get_rate(exchange, conversion_pairs).then(rate => {
+				possible_conversion_pairs[index] && possible_conversion_pairs[index].push(rate)
 			})
 			promises.push(promise)
 		})
@@ -40,7 +40,7 @@ function convert(exchange, from_currency, to_currency) {
 }
 
 function get_rate(exchange, conversion_pairs) {
-	promises = []
+	let promises = []
 	conversion_pairs.forEach(([pair, direction]) => {
 		promises.push(conversion_multiplier(exchange, pair, direction))
 	})
@@ -74,21 +74,21 @@ function conversion_multiplier(exchange, pair, direction) {
 }
 
 function find_conversion_pairs(pairs, from_currency, to_currency) {
-	intermediate_currency = 'BTC'
+	let intermediate_currency = 'BTC'
 
 	// e.g. from: USDT to: USDT
 	if(from_currency === to_currency) return [[]]
 
 	// e.g. from: BTC to: USDT, pair: BTC_USDT
-	pair = find_pair(pairs, from_currency, to_currency)
+	let pair = find_pair(pairs, from_currency, to_currency)
 	if (pair) return [[[pair, true]]]
 
 	// e.g. from: USDT to: BTC, pair: BTC_USDT
-	reverse_pair = find_pair(pairs, to_currency, from_currency)
+	let reverse_pair = find_pair(pairs, to_currency, from_currency)
 	if (reverse_pair) return [[[reverse_pair, false]]]
 
 	// e.g. from: XVG to: USDT, pairs: XVG_BTC, BTC_USDT
-	multiple_pairs = intersecting_pairs(find_from_pairs(pairs, from_currency), find_to_pairs(pairs, to_currency))
+	let multiple_pairs = intersecting_pairs(find_from_pairs(pairs, from_currency), find_to_pairs(pairs, to_currency))
 	multiple_pairs.forEach(([from_pair, to_pair], index) => {
 		multiple_pairs[index] = [[from_pair, true], [to_pair, true]]
 	})
@@ -99,7 +99,7 @@ function find_conversion_pairs(pairs, from_currency, to_currency) {
 
 function find_pair(pairs, from_currency, to_currency) {
   for (var i in pairs) {
-    pair = pairs[i].split('_')
+    let pair = pairs[i].split('_')
     if (pair[0] == from_currency && pair[1] == to_currency) {
       return pairs[i]
     }
@@ -107,13 +107,13 @@ function find_pair(pairs, from_currency, to_currency) {
 }
 
 function find_intermediate_pairs(pairs, from_currency, to_currency) {
-	to_pairs = find_to_pairs(pairs, to_currency)
-	from_pairs = find_from_pairs(pairs, from_currency)
+	let to_pairs = find_to_pairs(pairs, to_currency)
+	let from_pairs = find_from_pairs(pairs, from_currency)
 
 }
 
 function intersecting_pairs(from_pairs, to_pairs) {
-	ret = []
+	let ret = []
 	to_pairs.forEach(to_pair => {
 		from_pairs.forEach(from_pair => {
 			if (to_pair.split('_')[0] === from_pair.split('_')[1]) ret.push([from_pair, to_pair])
@@ -123,9 +123,9 @@ function intersecting_pairs(from_pairs, to_pairs) {
 }
 
 function find_to_pairs(pairs, to_currency) {
-	to_pairs = []
+	let to_pairs = []
   for (var i in pairs) {
-    pair = pairs[i].split('_')
+    let pair = pairs[i].split('_')
     if (pair[1] == to_currency) {
       to_pairs.push(pairs[i])
     }
@@ -134,9 +134,9 @@ function find_to_pairs(pairs, to_currency) {
 }
 
 function find_from_pairs(pairs, from_currency) {
-	from_pairs = []
+	let from_pairs = []
   for (var i in pairs) {
-    pair = pairs[i].split('_')
+    let pair = pairs[i].split('_')
     if (pair[0] == from_currency) {
       from_pairs.push(pairs[i])
     }
