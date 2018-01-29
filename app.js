6949b43a601654f973043dff0354e18fe7741aa1
Csaba Apagyi <csaba.apagyi@xing.com>
2018-01-29 18:02:06 +0100

    Removes unneeded files

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -21,9 +18,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
 
 var exchanges = require('./app/exchanges')
 app.use('/api/exchanges', exchanges.router)
